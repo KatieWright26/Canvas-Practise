6066db3e1fb9bacc959d57ebc99819b4d32b5333
katie.wrght@gmail.com <Katie.wrght@gmail.com>
2016-02-03 17:39:30 +1300

    click listener working

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 function Circle(positionX, positionY, radius, colorString) {
 	this.radius = radius
 	this.positionY = positionY
+	// console.log(positionY)
 	this.positionX = positionX
+	// console.log(positionX)
 	this.colorString = colorString
 }
 
-var circles = generateCircles(9)
-console.log(circles)
+var circles = generateCircles(6)
 
 function generateCircles(circlesPerBoard) {
 	var board = []
 	for (var i = 0; i < circlesPerBoard; i++) {
 		var row = []
 		for (var j = 0; j < circlesPerBoard; j++) {
-			row.push(new Circle(12.5+j*25,12.5+i*25, Math.PI*2, "rgb(90, 90, 0)"))
+			row.push(new Circle(12.5+j*25,12.5+i*25, 6, "rgb(" + Math.floor(255-20*i)+ "," + Math.floor(90-20*j) + ", 0)"))
 		}
 		board.push(row)
 	}
@@ -25,19 +26,25 @@ function clickIsInCircle(e) {
 
 	var xPosition = e.clientX
 	var yPosition = e.clientY
-
 	console.log(xPosition, yPosition)
-	for(var i = 0; i < circles.length; i++){
-		for(var j =0; j < circles.length; j++){
+
+
+	for(var i = 0; i < circles.length; i++) {
+		for(var j = 0; j < circles.length; j++) {
+
 			var circle = circles[i][j]
-			console.log(circle)
-			if((Math.pow((xPosition - circle.positionX), 2) - Math.pow((yPosition - circle.positionY), 2)) === Math.pow(circle.radius, 2)) {
-			console.log("click in circle")
-			return true
-		}	else { 
-			return false}
+
+			if((Math.pow((xPosition - circle.positionX - circle.radius), 2) + Math.pow((yPosition - circle.positionY - circle.radius), 2)) < Math.pow(circle.radius, 2)) {
+				console.log("click in circle")
+				console.log(circle)
+				return true
+			} else { 
+				console.log(xPosition, yPosition)
+			}
+
+		} 
 	}
-}
+	return false
 }
 
 function draw() { 
@@ -45,9 +52,8 @@ function draw() {
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d')
 		for(var i = 0; i < circles.length; i++){
-			for(var j =0; j < circles.length; j++){
+			for(var j = 0; j < circles.length; j++){
 				var circle = circles[i][j]
-				console.log(circle)
 				ctx.fillStyle = circle.colorString
 				ctx.beginPath()
 				ctx.arc(circle.positionX, circle.positionY, circle.radius, 0, Math.PI*2)
@@ -56,6 +62,6 @@ function draw() {
 		}
 	}
 
-	canvas.addEventListener('click', clickIsInCircle,false)
+	canvas.addEventListener('click', clickIsInCircle, false)
 
 }
